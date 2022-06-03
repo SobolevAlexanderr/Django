eb371500a09ae46efe68e1711d9efce653324069
1 <sobo754@mail.ru>
2022-06-03 15:12:13 +0400

    Initial

---
+++ blogsite/blogsite/urls.py
@@ -0,0 +1,12 @@
+"""
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('', include('frontend.urls'))
+]
